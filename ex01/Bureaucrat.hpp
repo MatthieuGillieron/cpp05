7b3201b9fe613a581f77de1ebd5bfc9cda2472ff
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-12-02 16:09:43 +0100

    add: New methode for bureaucrat class

--- ex01/Bureaucrat.hpp
+++ ex01/Bureaucrat.hpp
@@ -4,8 +4,7 @@
 
 #include <iostream>
 #include <string>
-// #include <stdexcept>
-
+#include "Form.hpp"
 
 class Bureaucrat 
 {
@@ -17,30 +16,32 @@ class Bureaucrat
 
 	public:
 
-	Bureaucrat();
-	Bureaucrat(const std::string& name, int grade);
-	Bureaucrat(const Bureaucrat& );
-	~Bureaucrat();
+		Bureaucrat();
+		Bureaucrat(const std::string& name, int grade);
+		Bureaucrat(const Bureaucrat& );
+		~Bureaucrat();
 
-	Bureaucrat& operator=(const Bureaucrat&);
+		Bureaucrat& operator=(const Bureaucrat&);
 
-	const std::string& getName() const;
-	int getGrade() const;
+		const std::string& getName() const;
+		int getGrade() const;
 
-	void	incrementGrade();
-	void	decrementGrade();
+		void	incrementGrade();
+		void	decrementGrade();
 
-	class GradeTooHighException : public std::exception
-	{
-		public:
-			virtual const char* what() const throw();
-	};
-	
-	class GradeTooLowException : public std::exception
-	{
-		public:
-			virtual const char* what() const throw();
-	};
+		void signForm(Form& form);
+
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
+		
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char* what() const throw();
+		};
 };
 
 std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
