c7432986b256ad609d4fadb0c0c96bd99b2dc9d1
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-12-09 11:48:08 +0100

    add: init file + todo

---
+++ ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   RobotomyRequestForm.cpp                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/09 11:39:03 by mg                #+#    #+#             */
+/*   Updated: 2025/12/09 11:46:25 by mg               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "RobotomyRequestForm.hpp"
+
+// TODO
+
+// canonique form
+// value defini : 
+// grade to sign -> 75
+// grade to exec -> 45
+// name -> "RobotomyRequestForm"
+
+
+// methode execute 
+// - checkRequierment -> call function for that
+// - bruit de perceuse -> log like "BZzzzz.."
+// - random luck -> 50% reuissite
+
