380e0dfda02c68fec31581d69cf45cdf15f37e46
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-12-10 14:32:24 +0100

    add: ALl test + pretty log

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,91 +6,99 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 10:41:55 by mg                #+#    #+#             */
-/*   Updated: 2025/12/08 13:47:23 by mg               ###   ########.fr       */
+/*   Updated: 2025/12/10 14:31:27 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
-
+#include "ShrubberyCreationForm.hpp"
+#include "RobotomyRequestForm.hpp"
+#include "PresidentialPardonForm.hpp"
 
 int main()
 {
-
-	Bureaucrat bob("bob", 10);
-
-
-
-
-    // std::cout << "=== Constructeurs valides ===" << std::endl;
-    // Bureaucrat sparrow("Sparrow", 1);
-    // Bureaucrat jack("Jack", 150);
-
-    // std::cout << sparrow << std::endl;
-    // std::cout << jack << std::endl;
-
-
-    // std::cout << "\n=== Constructeurs invalides ===" << std::endl;
-    // try 
-	// {
-    //     Bureaucrat tooHigh("tooHigh", 0);
-    // } 
-	// catch (std::exception& e)
-	// {
-    //     std::cout << e.what() << std::endl;
-    // }
-
-    // try
-	// {
-    //     Bureaucrat tooLow("tooLow", 151);
-    // }
-	// catch (std::exception& e)
-	// {
-    //     std::cout << e.what() << std::endl;
-    // }
-
-
-
-    // std::cout << "\n=== signature valides ===" << std::endl;
-    // try
-	// {
-	// 	Bureaucrat bob("bob", 1);
-	// 	Form formA("FormA", 10, 12);
-		
-	// 	std::cout << formA << std::endl;
-
-
-	// 	bob.signForm(formA);
-    //     std::cout << "\n" << formA << std::endl;
-    // }
 	
-	// catch (std::exception& e)
-	// {
-	// 	std::cout << e.what() << std::endl;
-    // }
+	srand(time(NULL)); 
 
+	std::cout << "\n=== FORM REQUIREMENTS ===" << std::endl;
 
-    // std::cout << "\n=== signature invalides ===" << std::endl;
-    // try
-	// {
-	// 	Bureaucrat boby("boby", 10);
-    // 	Form formB("FormB", 8, 4);
+	std::cout << "ShrubberyCreationForm :" << std::endl;
+	std::cout << "  - sign   : 145" << std::endl;
+	std::cout << "  - execute: 137" << std::endl;
 
-	// 	std::cout << formB << std::endl;
+	std::cout << "\nRobotomyRequestForm :" << std::endl;
+	std::cout << "  - sign   : 72" << std::endl;
+	std::cout << "  - execute: 45" << std::endl;
+
+	std::cout << "\nPresidentialPardonForm :" << std::endl;
+	std::cout << "  - sign   : 25" << std::endl;
+	std::cout << "  - execute: 5" << std::endl;
+
+	std::cout << "\n=== BUREAUCRAT GRADE ===" << std::endl;
 	
+	Bureaucrat chef("chef", 1);
+	Bureaucrat middle("middle", 72);
+	Bureaucrat noob("middle", 148);
 	
-	// 	boby.signForm(formB);
-	// 	std ::cout << boby << std::endl;
-
-    //     std::cout << "\n" << formB << std::endl;
-
-       
-    // }
+	std::cout << " - Chef: " << chef.getGrade() << std::endl;
+	std::cout << " - Middle: " << middle.getGrade() << std::endl;
+	std::cout << " - Noob: " << noob.getGrade() << std::endl;
 	
-	// catch (std::exception& e)
-	// {
-    //     std::cout << e.what() << std::endl;
-    // }
+	std::cout << "\n==========TEST============\n" << std::endl;
 
-    // return 0;
+	ShrubberyCreationForm shrub("A");
+    RobotomyRequestForm robo("B");
+    PresidentialPardonForm pardon("C");
+
+    std::cout << "\n [ CHEF ]\n";
+	std::cout << "\n===== Shrubbery =====\n";
+    chef.signForm(shrub);
+    chef.executeForm(shrub);
+
+	std::cout << "\n===== Robotomy =====\n";
+    chef.signForm(robo);
+    chef.executeForm(robo);
+
+	std::cout << "\n===== Presidential =====\n";
+    chef.signForm(pardon);
+    chef.executeForm(pardon);
+
+	
+    std::cout << "\n[ MIDDLE ]\n";
+    ShrubberyCreationForm shrub2("A2");
+    RobotomyRequestForm robo2("B2");
+    PresidentialPardonForm pardon2("C2");
+
+	std::cout << "\n===== Shrubbery =====\n";
+    middle.signForm(shrub2);
+    middle.executeForm(shrub2);
+
+	std::cout << "\n===== Robotomy =====\n";
+    middle.signForm(robo2);
+    middle.executeForm(robo2);
+
+	std::cout << "\n===== Presidential =====\n";
+    middle.signForm(pardon2);
+    middle.executeForm(pardon2);
+
+    std::cout << "\n[ NOOB ]\n";
+    ShrubberyCreationForm shrub3("A3");
+    RobotomyRequestForm robo3("B3");
+    PresidentialPardonForm pardon3("C3");
+
+	std::cout << "\n===== Shrubbery =====\n";
+    noob.signForm(shrub3);
+    noob.executeForm(shrub3);
+
+	std::cout << "\n===== Robotomy =====\n";
+    noob.signForm(robo3);
+    noob.executeForm(robo3);
+
+	std::cout << "\n===== Presidential =====\n";
+    noob.signForm(pardon3);
+    noob.executeForm(pardon3);
+
+    return 0;
+
 }
